4a5a0e874748ae157c67882bff8eff1ac0f7eda8
JLudt <jludt11@gmail.com>
2021-09-10 11:49:14 -0400

    completed part 1

--- app/models/tortoise.py
+++ app/models/tortoise.py
@@ -1,6 +1,7 @@
 #  /app/models/tortoise.py
 
 from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator  # new
 
 
 class TextSummary(models.Model):
@@ -10,3 +11,5 @@ class TextSummary(models.Model):
 
     def __str__(self):
         return self.url
+
+SummarySchema = pydantic_model_creator(TextSummary)  # new
